ddcc00bf912ba22a8da123e5be533c11c0136c4a
maxim1436 <36988345+maxim1436@users.noreply.github.com>
2021-10-25 18:05:21 +1000

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,22 +1,26 @@
-import Posts from './App.js';
+import { Posts, myPosts } from './app.js';
 
 document.getElementById('newPost').addEventListener('submit', (e) => {
-    let post = [{title: document.getElementById('newTitle').value, body: document.getElementById('newBody').value}];
+    let post = [{ title: document.getElementById('newTitle').value, body: document.getElementById('newBody').value }];
     createPost(post);
     e.preventDefault();
 })
 
-// document.getElementById().addEventListener('submit', (e) => {
-//     let post = [{title: document.getElementById('newTitle').value, body: document.getElementById('newBody').value}];
-//     createPost(post);
-//     e.preventDefault();
-// })
+document.getElementById('updatePost').addEventListener('submit', (e) => {
+    myPosts && myPosts.update(post.id, data);
+});
 
-function createPost(newPost){
+function createPost(newPost) {
     const post = new Posts(newPost);
     document.getElementById('newTitle').value = '';
     document.getElementById('newBody').value = '';
     post.print('postTitles');
 
 }
+
+document.getElementById('close').addEventListener('click', (e) => {
+    let a = document.getElementById('openModal');
+    a.style.display = "none";
+    e.preventDefault();
+})
 
