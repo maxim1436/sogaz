5fce3ed3b64940c9678ea12781df448513d5c408
maxim1436 <36988345+maxim1436@users.noreply.github.com>
2021-11-03 12:42:14 +1000

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,26 +1,98 @@
 import { Posts, myPosts } from './app.js';
 
+const url = 'https://jsonplaceholder.typicode.com/posts/';
+
 document.getElementById('newPost').addEventListener('submit', (e) => {
-    let post = [{ title: document.getElementById('newTitle').value, body: document.getElementById('newBody').value }];
-    createPost(post);
-    e.preventDefault();
-})
-
-document.getElementById('updatePost').addEventListener('submit', (e) => {
-    myPosts && myPosts.update(post.id, data);
-});
-
-function createPost(newPost) {
-    const post = new Posts(newPost);
+    let data = { title: document.getElementById('newTitle').value, body: document.getElementById('newBody').value };
+    myPosts.create(data);
     document.getElementById('newTitle').value = '';
     document.getElementById('newBody').value = '';
-    post.print('postTitles');
+    e.preventDefault();
+});
 
-}
+document.getElementById('updatePost').addEventListener('submit', (e) => {
+    let data = { title: document.getElementById('updateTitle').value, body: document.getElementById('updateBody').value };
+    myPosts && myPosts.update(data);
+    let modalWindow = document.getElementById('openModal');
+    modalWindow.style.display = "none";
+    document.getElementById('updateTitle').value = '';
+    document.getElementById('updateBody').value = '';
+    e.preventDefault();
+});
 
 document.getElementById('close').addEventListener('click', (e) => {
     let a = document.getElementById('openModal');
     a.style.display = "none";
+    document.getElementById('updateTitle').value = '';
+    document.getElementById('updateBody').value = '';
     e.preventDefault();
-})
+});
 
+document.getElementById('search').oninput = function() {
+    if (document.getElementById('filterList') !== null) document.getElementById('filterList').remove();
+    const postTitles = document.getElementById('postTitles');
+    if (document.getElementById('search').value.length >= 1) {
+        const filterList = document.createElement('ol');
+        filterList.setAttribute('id', 'filterList');
+        document.getElementById('myLists').append(filterList);
+        const array = myPosts.posts.filter((i) => i.title.includes(document.getElementById('search').value));
+        console.log(array);
+        postTitles.style.display = 'none';
+        array.forEach(post => {
+            let id = post.id;
+            const element = document.createElement('li');
+            element.innerHTML = post.title;
+            element.setAttribute('name', id);
+            document.getElementById('filterList').append(element);
+            deletePostButton(id);
+            updatePostButton(id);
+        });
+    } else {
+        postTitles.style.display = 'block';
+    }
+};
+
+export async function getPosts() {
+    const response = await fetch(url);
+    return await response.json();
+};
+
+export function createId() {
+    return Math.random().toString(16);
+};
+
+export function createObjectsArray(posts) {
+    return posts.map(i => ({
+        title: i.title,
+        body: i.body,
+        isUpdating: false,
+        id: createId()
+    }))
+};
+
+export function deletePostButton(id) {
+    const listItem = document.getElementById(id);
+    const deleteElement = document.createElement('button');
+    deleteElement.innerHTML = 'X';
+    deleteElement.addEventListener('click', (e) => {
+        myPosts.delete(id);
+        document.getElementById(id).remove();
+        e.preventDefault();
+    });
+    listItem.append(deleteElement);
+};
+
+export function updatePostButton(id) {
+    const listItem = document.getElementById(id);
+    const updateElement = document.createElement('button');
+    updateElement.innerHTML = 'update';
+    updateElement.addEventListener('click', (e) => {
+        const modalWindow = document.getElementById('openModal');
+        modalWindow.style.display = "block";
+        myPosts.find(id).isUpdating = true;
+        document.getElementById('updateTitle').value = myPosts.find(id).title;
+        document.getElementById('updateBody').value = myPosts.find(id).body;
+        e.preventDefault();
+    });
+    listItem.append(updateElement);
+};
